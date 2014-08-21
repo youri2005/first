732120e2315cab48fc8fbed051b5486cf6a5bdac
Yuriy <youri2005@mail.ru>
2014-08-22 00:35:19 +0300

    sort

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,14 +8,8 @@ public class Main {
     public static void main(String[] args) {
         Student studentNew= new Student();
         Course Wow= new Course();
-<<<<<<< HEAD
         studentNew.name = "Vasia";
-        studentNew.course.name ="Trud11";
-=======
-        studentNew.name = "Vasirrrra";
         studentNew.course.name ="Trud";
->>>>>>> d902c7f976069ed4e84bedfe6c9586b9a04beea6
-
         studentNew.sayHello();
         studentNew.course.aboutCourse();
         Student studentOld = new Student();
