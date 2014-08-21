3b6de0bba7c611f51b682b7eeb95afc839cb5a0f
Yuriy <youri2005@mail.ru>
2014-08-21 21:26:37 +0300

    test2

--- src/com/company/Common/Course.java
+++ src/com/company/Common/Course.java
@@ -2,7 +2,7 @@ package com.company.Common;
 
 public class Course {
     public String name= "n/a";
-    public int hourse= 1;
+    public int hourse= 1333;
 
     public void aboutCourse () {
        System.out.println("Course name "+ name);
