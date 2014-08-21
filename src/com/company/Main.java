b9076b0c0dee8c40f2474d633b623fdac9fe445c
Yuriy <youri2005@mail.ru>
2014-08-22 00:00:21 +0300

    test2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         Student studentNew= new Student();
         Course Wow= new Course();
-        studentNew.name = "Vasia";
+        studentNew.name = "Vasirrrra";
         studentNew.course.name ="Trud";
 
         studentNew.sayHello();
